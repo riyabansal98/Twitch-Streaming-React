c65c6d0c93026405923f2341eec46dd32b8d096c
riyabansal98 <riyabansal98@gmail.com>
2019-06-04 16:46:06 +0530

    Implemented Player Building

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -11,7 +11,25 @@ class StreamShow extends React.Component{
         this.videoRef = React.createRef();
     }
     componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id);
+
+        const { id } = this.props.match.params;
+        this.props.fetchStream(id);
+       
+    }
+
+    buildPlayer(){
+        if(this.player || !this.props.stream){
+            return;
+        }
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+
     }
     render(){
         if(!this.props.stream){
